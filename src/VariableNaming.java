04521f3231e7ba55f4d7dfd9c9376e2040523862
Ashamango <ashaanupdas@gmail.com>
2023-06-03 21:04:57 -0400

    Declared variables and initialized each of these variables

---
+++ src/VariableNaming.java
@@ -0,0 +1,18 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class VariableNaming {
+    public static void main(String[] args) {
+        // Declare variables
+        int kids = 6;
+        int birth = 3;
+        // Declare double variables
+        double myLunchCost = 12.50;
+        double gas = 3.50;
+        double favoriteNumber = 7;
+        double shoeSize = 9.5;
+        // Declare string variables
+        String Asha_Das;
+        String is_It_Raining = String.valueOf(false);
+
+    }
+}
